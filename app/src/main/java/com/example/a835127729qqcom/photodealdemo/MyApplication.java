543acd56f26f9190edb02c0487f8045747943adc
kaiyi.cky <kaiyi.cky@alibaba-inc.com>
2016-09-27 16:10:04 +0800

    增加内存泄漏检查

---
+++ app/src/main/java/com/example/a835127729qqcom/photodealdemo/MyApplication.java
@@ -0,0 +1,16 @@
+package com.example.a835127729qqcom.photodealdemo;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by 835127729qq.com on 16/9/27.
+ */
+public class MyApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
