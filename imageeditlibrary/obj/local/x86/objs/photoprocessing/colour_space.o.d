ed2ca6c16c24ff694a1cd4db10c38a62c13271ca
kaiyi.cky <kaiyi.cky@alibaba-inc.com>
2016-09-28 17:31:24 +0800

    del imagelib finally, this project live alone

--- imageeditlibrary/obj/local/x86/objs/photoprocessing/colour_space.o.d
+++
@@ -1,5 +0,0 @@
-/Users/835127729qq.com/Documents/ImageEditor-Android/imageeditlibrary/obj/local/x86/objs/photoprocessing/colour_space.o: \
- /Users/835127729qq.com/Documents/ImageEditor-Android/imageeditlibrary/jni/colour_space.c \
- /Users/835127729qq.com/Documents/ImageEditor-Android/imageeditlibrary/jni/colour_space.h
-
-/Users/835127729qq.com/Documents/ImageEditor-Android/imageeditlibrary/jni/colour_space.h:
--- imageeditlibrary/obj/local/x86_64/objs/photoprocessing/nanojpeg.o.d
+++
@@ -1,2 +0,0 @@
-/Users/835127729qq.com/Documents/ImageEditor-Android/imageeditlibrary/obj/local/x86_64/objs/photoprocessing/nanojpeg.o: \
- /Users/835127729qq.com/Documents/ImageEditor-Android/imageeditlibrary/jni/nanojpeg.c
