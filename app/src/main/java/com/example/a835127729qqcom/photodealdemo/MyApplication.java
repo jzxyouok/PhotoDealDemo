64cfa915015b06177c578209f67722d7b933c1ce
kaiyi.cky <kaiyi.cky@alibaba-inc.com>
2016-10-13 16:32:38 +0800

    减少绘制过程中new操作

--- app/src/main/java/com/example/a835127729qqcom/photodealdemo/MyApplication.java
+++ app/src/main/java/com/example/a835127729qqcom/photodealdemo/MyApplication.java
@@ -2,8 +2,6 @@ package com.example.a835127729qqcom.photodealdemo;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 /**
  * Created by 835127729qq.com on 16/9/27.
  */
